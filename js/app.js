99c0d9bc1bdb1d8a4d7b5f7b071a3bf613654a80
NostromO1975 <fantasmadln@gmail.com>
2025-04-17 22:37:37 -0500

    Añadida lista completa de frutas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 // Aquí irá la lógica de la aplicación
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Aplicación cargada');
-    
+    document.getElementById('fruta').addEventListener('change', function() {
+        const otroContainer = document.getElementById('otro-fruta-container');
+        if (this.value === 'otro') {
+            otroContainer.style.display = 'block';
+        } else {
+            otroContainer.style.display = 'none';
+        }
+    });
     // Eventos y funciones se agregarán aquí
 });
