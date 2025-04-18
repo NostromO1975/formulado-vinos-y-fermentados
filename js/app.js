b75e223bbf71e583cf095ef43142e0884bcf94ba
NostromO1975 <fantasmadln@gmail.com>
2025-04-17 22:31:30 -0500

    Creación estructura inicial

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Aquí irá la lógica de la aplicación
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Aplicación cargada');
+    
+    // Eventos y funciones se agregarán aquí
+});
